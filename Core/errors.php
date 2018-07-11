d5f412573c95316ef8524966c9253b1822fe45c8
Mehdi Bounya <contact.mehdi@pm.me>
2018-07-11 11:32:35 +0100

    Add rules errors

---
+++ Core/errors.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Error messages for validation rules
+ *
+ * {field} will be replaced with the field's name
+ * {params} will be replaced with any passed arguments (inList[arg1,arg2...])
+ */
+return [
+    'required'    => 'The {field} field is required.',
+    'inList'      => 'The {field} field must be one of these options: {params}.',
+    'minLength'   => 'The {field} field must be at least {params} characters long.',
+    'maxLength'   => 'The {field} field must not be more than {params} characters long.',
+    'exactLength' => 'The {field} field must be exactly {params} characters long.',
+    'greaterThan' => 'The {field} field must be bigger than {params}.',
+    'lessThan'    => 'The {field} field must be less than {params}.',
+    'validEmail'  => 'The {field} field must be a valid e-mail.',
+];
