d94e0411117251a0d0fbea10a454fdfa7c13c91e
Mehdi Bounya <contact.mehdi@pm.me>
2018-07-12 16:57:32 +0100

    Add ExportCSV

---
+++ Core/ExportCSV.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace PHPForms;
+
+class ExportCSV implements Exporter
+{
+    /**
+     * CSV file path
+     *
+     * @var string
+     */
+    private $file_path;
+
+    /**
+     * Data to export
+     *
+     * @var array
+     */
+    private $data;
+
+    /**
+     * Fields map
+     *
+     * @var array
+     */
+    private $map;
+
+    /**
+     *
+     * @param string $file_path CSV File path
+     * @param array $data Data to write
+     * @param array $map Fields map, an array of 'field_name' => 'column name'
+     */
+    public function __construct(string $file_path, array $data = [], array $map = [])
+    {
+        $this->file_path = $file_path;
+        $this->data = $data;
+        $this->map = $map;
+    }
+
+    /**
+     * Add data to export
+     *
+     * @param string $name  Column name
+     * @param string $value Value
+     * @return void
+     */
+    public function addData(string $name, string $value):void
+    {
+        $this->data[$name] = $value;
+    }
+
+    /**
+     * Set map for fields
+     *
+     * Map fields to different columns
+     *
+     * @param array $map An array of 'field_name' => 'column_name'
+     * @return void
+     */
+    public function setMap(array $map):void
+    {
+        $this->map = $map;
+    }
+
+    /**
+     * Export data
+     *
+     * @param bool $append TRUE to append data if the file exists and false to write a new one
+     * @return boolean TRUE if exported successfully or FALSE otherwise
+     */
+    public function export($append = true):bool
+    {
+        // If the file doesn't exist or $append is false create a new file
+        if (!file_exists($this->file_path) || !$append) {
+            return $this->newFile();
+        }
+
+        /**
+         * Append the data to the file
+         * We need to get the columns order from the file in case the data passed
+         * Is not in the same order as the file
+         */
+        $file = fopen($this->file_path, 'a+');
+
+        if (!$file) {
+            return false;
+        }
+
+        $line = trim(fgets($file));
+        $columns = str_getcsv($line);
+
+        // Get the original field name if any
+        if (!empty($this->map)) {
+            $columns = array_map(function ($column) {
+                $key = array_search($column, $this->map);
+                return ($key === false) ? $column : $key;
+            }, $columns);
+        }
+
+        // Loop through the columns
+        $data = [];
+        foreach ($columns as $column) {
+            $data[] = (empty($this->data[$column])) ? '' : $this->data[$column];
+        }
+
+        return fputcsv($file, $data);
+    }
+
+    /**
+     * Create a new CSV file and put data into it
+     *
+     * @return boolean TRUE if created successfully FALSE otherwise
+     */
+    private function newFile():bool
+    {
+        $file = fopen($this->file_path, 'w');
+
+        if ($file === false) {
+            return false;
+        }
+
+        // Get the fields and data
+        $fields = [];
+        $data = [];
+        foreach ($this->data as $field => $value) {
+            $fields[] = (empty($this->map[$field])) ? $field : $this->map[$field];
+            $data[] = $value;
+        }
+
+        // Put them into the file
+        return !(fputcsv($file, $fields) === false || fputcsv($file, $data) === false);
+    }
+}
