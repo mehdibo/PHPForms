e7523a0e0cf70bc5f08280b0ed25e1b5e394a16f
Mehdi Bounya <contact.mehdi@pm.me>
2018-07-12 09:43:08 +0100

    Add thorough testing

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -26,119 +26,173 @@ class ValidatorTest extends \PHPUnit\Framework\TestCase
     public function setUp()
     {
         $fields = [
-            'first_name' => [
-                'label' => 'First name',
-                'type'  => 'text',
-                'rules' => 'required|inList[option1,option2]'
-            ],
-            'last_name' => [
-                'label' => 'Last name',
+            'label_exists' => [
+                'label' => 'Label exists',
                 'type'  => 'text',
                 'rules' => 'required'
             ],
-            'street' => [
-                'label' => 'Street',
-                'type'  => 'test',
-                'rules' => 'required|inList[valid1,valid2]'
-            ]
+            'label_not_exists' => [
+                'type'  => 'text',
+                'rules' => 'required'
+            ],
+            'required_options' => [
+                'label'  => 'Required options',
+                'type'  => 'text',
+                'rules' => 'required|inList[option1,option2]'
+            ],
+            'optional_options' => [
+                'label'  => 'Optional options',
+                'type'  => 'text',
+                'rules' => 'inList[option1,option2]'
+            ],
         ];
 
         $this->form = new \PHPForms\Form($fields);
-        $errors = ['required' => 'Required error msg.', 'inList' => 'inList error msg.'];
+        $errors = [
+            'required' => '{field} is required',
+            'inList' => '{field} must be one of these options: {params}',
+        ];
         $this->validations = new \PHPForms\Validations($errors);
     }
 
     public function testValidationIsWorking()
     {
-        // Validate should return TRUE because the rules were respected
-        $data = [
-            'first_name' => 'option1',
-            'last_name'  => 'Bounya',
-            'street'  => 'valid1',
+        $tests = [
+            [
+                'data' => [
+                    'label_exists'      => 'This field is required',
+                    'label_not_exists'  => 'Also this',
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+                'expected_result' => true,
+            ],
+            [
+                'data' => [
+                    'label_exists'      => 'Required',
+                    'label_not_exists'  => 'Required too',
+                    'required_options'  => 'option1',
+                    'optional_options'  => 'option2',
+                ],
+                'expected_result' => true,
+            ],
+            [
+                'data' => [
+                    'label_exists'      => '',
+                    'label_not_exists'  => 'Also this',
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+                'expected_result' => false,
+            ],
+            [
+                'data' => [
+                    'label_exists'      => '',
+                    'label_not_exists'  => 'Also this',
+                    'required_options'  => 'option1',
+                    'optional_options'  => 'invalid_option',
+                ],
+                'expected_result' => false,
+            ],
         ];
 
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $this->assertEquals($validator->validate(), true);
-
-        // Validate should return FALSE because the rules were not respected
-        $data = [
-            'first_name' => 'option3',
-            'last_name'  => 'Bounya',
-            'street'  => 'valid1',
-        ];
-
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $this->assertEquals($validator->validate(), false);
+        foreach ($tests as $test) {
+            $validator = new \PHPForms\Validator($this->form, $test['data'], $this->validations);
+            $this->assertEquals($test['expected_result'], $validator->isValid());
+        }
     }
 
     public function testWeCanGetValidData()
     {
-        // We should get a part of the data because it passed the rules
-        $data = [
-            'first_name' => 'option1',
-            'last_name'  => 'Bou',
-            'street' => 'invalid option',
+        $tests = [
+            [
+                'data' => [
+                    'label_exists'      => 'This field is required',
+                    'label_not_exists'  => 'Also this',
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+                'expected_result' => [
+                    'label_exists'      => 'This field is required',
+                    'label_not_exists'  => 'Also this',
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+            ],
+            [
+                'data' => [
+                    'label_exists'      => '',
+                    'label_not_exists'  => '',
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+                'expected_result' => [
+                    'required_options'  => 'option1',
+                    'optional_options'  => '',
+                ],
+            ],
+            [
+                'data' => [
+                    'label_exists'      => '',
+                    'label_not_exists'  => '',
+                    'required_options'  => '',
+                    'optional_options'  => 'invalid_option',
+                ],
+                'expected_result' => [],
+            ],
         ];
 
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $validator->validate();
-
-        $expected_data = [
-            'first_name' => 'option1',
-            'last_name' => 'Bou',
-        ];
-
-        $this->assertEquals($validator->getValidData(), $expected_data);
-
-        // No data should be returned because we didn't respect any rules
-        $data = [
-            'first_name' => 'invalid option',
-            'last_name'  => '',
-            'street' => 'invalid option',
-        ];
-
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $validator->validate();
-
-        $expected_data = [];
-
-        $this->assertEquals($validator->getValidData(), $expected_data);
+        foreach ($tests as $test) {
+            $validator = new \PHPForms\Validator($this->form, $test['data'], $this->validations);
+            $validator->isValid();
+            $this->assertEquals($test['expected_result'], $validator->getValidData());
+        }
     }
 
     public function testWeCanGetErrorMessages()
     {
-        // We should get two error messages because some data didn't pass the rules
-        $data = [
-            'first_name' => 'option1',
-            'last_name'  => '',
-            'street' => 'invalid option',
+        $tests = [
+            [
+                'data' => [
+                    'label_exists'      => '',
+                    'label_not_exists'  => '',
+                    'required_options'  => '',
+                    'optional_options'  => 'invalid_option',
+                ],
+                'expected_result' => [
+                    'Label exists is required',
+                    'label_not_exists is required',
+                    'Required options is required',
+                    'Optional options must be one of these options: option1,option2',
+                ],
+            ],
+            [
+                'data' => [
+                    'label_exists'      => 'Required field',
+                    'label_not_exists'  => 'Required field 2',
+                    'required_options'  => 'option1',
+                    'optional_options'  => 'option1',
+                ],
+                'expected_result' => [],
+            ],
+            [
+                'data' => [
+                    'label_exists'      => 'Required field',
+                    'label_not_exists'  => '',
+                    'required_options'  => 'invalid_option',
+                    'optional_options'  => '',
+                ],
+                'expected_result' => [
+                    'label_not_exists is required',
+                    'Required options must be one of these options: option1,option2',
+                ],
+            ],
         ];
 
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $validator->validate();
-
-        $expected_data = ['Required error msg.', 'inList error msg.'];
-
-        $this->assertEquals($validator->getErrors(), $expected_data);
-
-        // No error message because the rules were respected
-        $data = [
-            'first_name' => 'option1',
-            'last_name'  => 'X',
-            'street' => 'valid1',
-        ];
-
-        $validator = new \PHPForms\Validator($this->form, $data, $this->validations);
-
-        $validator->validate();
-
-        $expected_data = [];
-
-        $this->assertEquals($validator->getErrors(), $expected_data);
+        foreach ($tests as $test) {
+            $validator = new \PHPForms\Validator($this->form, $test['data'], $this->validations);
+            $validator->isValid();
+            $this->assertEquals($test['expected_result'], $validator->getErrors());
+        }
     }
 }
